177b34cb37dcb0a6008b736d5e2296bee5d7ce88
c9palmil <c9palmil@vasvari.org>
2024-11-14 10:18:49 +0100

    gam

---
+++ fuckganin/04-00-00-wpf-mvvm-design-menu-main/KretaBasicSchoolSystem.Desktop/ViewModels/MainViewModel.cs
@@ -0,0 +1,86 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FontAwesome.Sharp;
+using KretaBasicSchoolSystem.Desktop.ViewModels.Base;
+using KretaBasicSchoolSystem.Desktop.ViewModels.ControlPanel;
+using KretaBasicSchoolSystem.Desktop.ViewModels.SchoolCitizens;
+using KretaBasicSchoolSystem.Desktop.ViewModels.SchoolClasses;
+using KretaBasicSchoolSystem.Desktop.ViewModels.SchoolSubjects;
+
+namespace KretaBasicSchoolSystem.Desktop.ViewModels
+{
+    public partial class MainViewModel : BaseViewModel
+    {
+        private ControlPanelViewModel _controlPanelViewModel;
+        private SchoolCitizensViewModel _schoolCitizensViewModel;
+        private SchoolClassesViewModel _schoolClassesViewModel;
+        private SchoolSubjectsViewModel _schoolSubjectsViewModel;
+
+        public MainViewModel()
+        {
+            _controlPanelViewModel = new ControlPanelViewModel();
+            _schoolCitizensViewModel = new SchoolCitizensViewModel();
+            _schoolClassesViewModel = new SchoolClassesViewModel();
+            _schoolSubjectsViewModel = new SchoolSubjectsViewModel();
+        }
+
+        public MainViewModel(
+            ControlPanelViewModel controlPanelViewModel,  // golf ütő - dependency injection
+            SchoolCitizensViewModel schoolCitizensViewModel,
+            SchoolClassesViewModel schoolClassesViewModel,
+            SchoolSubjectsViewModel schoolSubjectsViewModel
+            )
+        {
+            _controlPanelViewModel = controlPanelViewModel;
+            _schoolCitizensViewModel = schoolCitizensViewModel;
+            _schoolClassesViewModel= schoolClassesViewModel;
+            _schoolSubjectsViewModel= schoolSubjectsViewModel;
+
+
+            // CurrentChildView = _controlPanelViewModel; // amikor elindul a program legyen kiválasztott menüpont
+            ShowDashbord();
+        }
+
+        [ObservableProperty]
+        private string _caption = string.Empty;
+
+        [ObservableProperty]
+        private IconChar _icon = new IconChar();
+
+        [ObservableProperty]
+        private BaseViewModel _currentChildView;
+
+        [RelayCommand]
+        public void ShowDashbord()
+        {
+            Caption = "Vezérlőpult";
+            Icon=IconChar.SolarPanel;
+            CurrentChildView = _controlPanelViewModel;
+        }
+
+        [RelayCommand]
+        public void ShowSchoolCitizens()
+        {
+            Caption = "Iskolapolgárok";
+            Icon = IconChar.UserGroup;
+            CurrentChildView = _schoolCitizensViewModel;
+        }
+
+        // SchoolClass menüpont meghívása
+        [RelayCommand]
+        public void ShowSchoolClasses()
+        {
+            Caption = "Osztályok";
+            Icon = IconChar.ChalkboardUser;
+            CurrentChildView = _schoolClassesViewModel;
+        }
+
+        [RelayCommand]
+        public void ShowSchoolSubjects()
+        {
+            Caption = "Tantárgyak";
+            Icon = IconChar.ChalkboardUser;
+            CurrentChildView = _schoolSubjectsViewModel;
+        }
+    }
+}
